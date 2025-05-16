d9e2e7fe7b4cd04d7301aa5caa697a941fe8343c
stagiaire1 <stagiaire1@pil-media.local>
2025-05-16 13:23:29 -0400

    Ajout section pourquoi nous + responsive

--- backend/main/models.py
+++ backend/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Realisation(models.Model):
+    nom_projet = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='static/image/', blank=True, null=True)
+
+    def __str__(self):
+        return self.nom_projet
